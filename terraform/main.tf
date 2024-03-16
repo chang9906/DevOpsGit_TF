cdc046400b7c26244a25c87626844cbb91759ae0
Changjun Kim <changjun99@gmail.com>
2024-03-16 17:42:42 -0400

    removed unnecessary attributes egress

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,12 +41,8 @@ resource "aws_security_group" "SG1" {
     egress = [
         {
             cidr_blocks = ["0.0.0.0/0"]
-            description = ""
             from_port = 0
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
             protocol = "-1"
-            security_groups = []
             self = false
             to_port = 0
         }
