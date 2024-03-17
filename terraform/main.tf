ea5c0c32cc1b4d4879d0e71ab0fb107ccce9d906
Changjun Kim <changjun99@gmail.com>
2024-03-17 13:23:14 -0400

    commented key_pair resource block

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "SG1" {
         }
     ]
 }
-resource "aws_key_pair" "deployer"{
+/*resource "aws_key_pair" "deployer"{
     key_name = var.key_name
     public_key = var.public_key
-}
+}*/
